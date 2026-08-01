305e6cf38f2340a28de967a4570ee686827e5459
rohitraomanim <manimrohitrao26@gmail.com>
2026-08-01 09:27:19 +0530

    Add files via upload

---
+++ 1950A_Stair, Peak, or neither.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+
+    for(int i = 0; i < n; i++) {
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int c = sc.nextInt();
+
+      if(a < b && b < c) {
+        System.out.println("STAIR");
+      }
+      else if(b > a && b > c) {
+        System.out.println("PEAK");
+      }
+      else {
+        System.out.println("NONE");
+      }
+    }
+  }
+}
