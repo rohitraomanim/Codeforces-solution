dce9f4cb9667630287e770ffd438c7d7005199d4
rohitraomanim <manimrohitrao26@gmail.com>
2026-08-19 17:39:01 +0530

    Add files via upload

---
+++ 467A_George And Acommodation.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    int count = 0;
+
+    for(int i = 0; i < n; i++) {
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+
+      int c = b - a;
+
+      if(c >= 2) {
+        count++;
+      }
+    }
+
+    System.out.println(count);
+  }
+}
