ce84ae5103f797dea742599ca80dd618ca92aa95
Manim Rohit Rao <manimrohitrao26@gmail.com>
2026-09-06 12:11:19 +0530

    Add files via upload

---
+++ 200B_Drinks.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    double sum = 0;
+    for(int i = 0; i < n; i++) {
+      int p = sc.nextInt();
+      sum += p;
+    }
+
+    double res = sum / n;
+
+    System.out.println(res);
+  }
+}  
