af6a5340d1d4c985b6db84eb6814ed2449e8a759
Manim Rohit Rao <manimrohitrao26@gmail.com>
2026-09-16 23:37:39 +0530

    Add files via upload

---
+++ 1899A_Game with Integers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+
+    while(t-- > 0) {
+      int n = sc.nextInt();
+      
+      if(n % 3 != 0) {
+        System.out.println("First");
+      }
+      else {
+        System.out.println("Second");
+      }
+    }
+  }
+}
